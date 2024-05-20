b80fe992c633307ddc533bc14c5b0cae45db90ae
Akmalbekov070 <akmalbekov2022@gmail.com>
2024-05-20 22:08:41 +0300

    commit server base

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ dotenv.config();
 
 const app = express();
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 
 app.listen(PORT, () => {
 	console.log(`SERVER ${PORT}`);
