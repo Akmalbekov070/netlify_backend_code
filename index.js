f8bdbce3f41f466376ea8bdf1c61a3bb3f5b337f
Akmalbekov070 <akmalbekov2022@gmail.com>
2024-07-18 11:41:38 +0300

    apload page

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 const express = require('express');
 const dotenv = require('dotenv');
+const exphbs = require('express-handlebars');
 dotenv.config();
 
 const app = express();
 
 const PORT = process.env.PORT || 3001;
+
+//intializing
+app.engine('.hbs', exphbs.engine({ extname: 'hbs' }));
+app.set('view engine', '.hbs');
 
 app.listen(PORT, () => {
 	console.log(`SERVER ${PORT}`);
